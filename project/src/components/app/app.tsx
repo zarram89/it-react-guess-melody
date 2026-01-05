90f98cac00e544e80001406002418e11c092f13f
Ramil <zarram89@gmail.com>
2026-01-05 19:41:23 +0500

    3.5. Заменит Route на PrivateRoute. Защитим маршрут `/result` при помощи компонента `PrivateRoute`. Если пользователь прошёл авторизацию, то он попадёт на маршрут `/result`. Если нет, то будет перенаправлен на `/login`.
    На данном этапе будем передавать статичное значение для пропса
    `authorizationStatus`. Пользователь всегда будет считаться
    неавторизованным. Мы исправим это, когда научимся взаимодействовать
    с сервером.

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,5 +1,5 @@
 import {Route, BrowserRouter, Routes} from 'react-router-dom';
-import {AppRoute} from '../../const';
+import {AppRoute, AuthorizationStatus} from '../../const';
 import WelcomeScreen from '../../pages/welcome-screen/welcome-screen';
 import ArtistQuestionScreen from '../../pages/artist-question-screen/artist-question-screen';
 import GenreQuestionScreen from '../../pages/genre-question-screen/genre-question-screen';
@@ -7,6 +7,7 @@ import AuthScreen from '../../pages/auth-screen/auth-screen';
 import GameOverScreen from '../../pages/game-over-screen/game-over-screen';
 import WinScreen from '../../pages/win-screen/win-screen';
 import NotFoundScreen from '../../pages/not-found-screen/not-found-screen';
+import PrivateRoute from '../private-route/private-route';
 
 type AppScreenProps = {
   errorsCount: number;
@@ -18,31 +19,37 @@ function App({errorsCount}: AppScreenProps): JSX.Element {
       <Routes>
         <Route
           path={AppRoute.Root}
-          element={<WelcomeScreen errorsCount={errorsCount} />}
+          element={<WelcomeScreen errorsCount={errorsCount}/>}
         />
         <Route
           path={AppRoute.DevArtist}
-          element={<ArtistQuestionScreen />}
+          element={<ArtistQuestionScreen/>}
         />
         <Route
           path={AppRoute.DevGenre}
-          element={<GenreQuestionScreen />}
+          element={<GenreQuestionScreen/>}
         />
         <Route
           path={AppRoute.Login}
-          element={<AuthScreen />}
+          element={<AuthScreen/>}
         />
         <Route
           path={AppRoute.Result}
-          element={<WinScreen />}
+          element={
+            <PrivateRoute
+              authorizationStatus={AuthorizationStatus.NoAuth}
+            >
+              <WinScreen />
+            </PrivateRoute>
+          }
         />
         <Route
           path={AppRoute.Lose}
-          element={<GameOverScreen />}
+          element={<GameOverScreen/>}
         />
         <Route
           path="*"
-          element={<NotFoundScreen />}
+          element={<NotFoundScreen/>}
         />
       </Routes>
     </BrowserRouter>
