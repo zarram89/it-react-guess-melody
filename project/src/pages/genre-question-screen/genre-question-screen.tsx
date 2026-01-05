eada57528231f0fb7e90d5921d9d71e9c856f383
Ramil <zarram89@gmail.com>
2026-01-05 19:24:45 +0500

    3.1. Добавит маршрутизацию. На этом шаге добавим маршрутизацию в приложение. Для этого в модуле компонента `App` подключим несколько компонентов из пакета `react-router-dom` (`Routes`, `Route`, `BrowserRouter`), а затем обновим код самого компонента: добавим маршрутизацию.
    Обратите внимание: маршруты `/dev-artist`, `/dev-genre` нужны только
    на этапе разработки. Позже мы избавимся от них, заменив маршрутом
    `/game`.

--- project/src/pages/genre-question-screen/genre-question-screen.tsx
+++ project/src/pages/genre-question-screen/genre-question-screen.tsx
@@ -8,9 +8,7 @@ function GenreQuestionScreen(): JSX.Element {
         </a>
 
         <svg xmlns="http://www.w3.org/2000/svg" className="timer" viewBox="0 0 780 780">
-          <circle className="timer__line" cx="390" cy="390" r="370"
-                  style={{filter: 'url(#blur)', transform: 'rotate(-90deg) scaleY(-1)', transformOrigin: 'center'}}
-          />
+          <circle className="timer__line" cx="390" cy="390" r="370" style={{filter: 'url(#blur)', transform: 'rotate(-90deg) scaleY(-1)', transformOrigin: 'center'}}/>
         </svg>
 
         <div className="game__mistakes">
--- project/src/pages/not-found-screen/not-found-screen.tsx
+++ project/src/pages/not-found-screen/not-found-screen.tsx
@@ -4,13 +4,11 @@ function NotFoundScreen(): JSX.Element {
       <header className="game__header">
         <a className="game__back" href="/">
           <span className="visually-hidden">Сыграть ещё раз</span>
-          <img className="game__logo" src="img/melody-logo-ginger.png" alt="Угадай мелодию" />
+          <img className="game__logo" src="img/melody-logo-ginger.png" alt="Угадай мелодию"/>
         </a>
 
         <svg xmlns="http://www.w3.org/2000/svg" className="timer" viewBox="0 0 780 780">
-          <circle className="timer__line" cx="390" cy="390" r="370"
-                  style={{filter: 'url(#blur)', transform: 'rotate(-90deg) scaleY(-1)', transformOrigin: 'center'}}
-          />
+          <circle className="timer__line" cx="390" cy="390" r="370" style={{filter: 'url(#blur)', transform: 'rotate(-90deg) scaleY(-1)', transformOrigin: 'center'}}/>
         </svg>
       </header>
 
